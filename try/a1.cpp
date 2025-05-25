99dd591e00ff7de08505d7b72a25ae20cb3a3d14
CSSYJ-awa <13710671467@163.com>
2025-05-25 13:20:47 +0800

    Add .gitignore and exclude binary files

--- try/a1.cpp
+++ try/a1.cpp
@@ -1,16 +1,29 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
 int main() {
-    float num1, num2;
-    char op;
-    cin>>num1>>num2>>op;
+    // 变量声明部分
+    float num1, num2;  // 存储两个操作数
+    char op;           // 存储运算符
+    
+    cin >> num1 >> num2 >> op;  // 连续输入两个数字和一个运算符
+
+    // 运算符判断结构
     switch(op) {
-        case '+': cout<<num1+num2; break;
-        case '-': cout<<num1-num2; break;
-        case '*': cout<<num1*num2; break;
-        case '/': cout<<num1/num2; break;
-        default: cout<<"Invalid operator"; break;
+        case '+': 
+            cout << num1 + num2;  // 加法
+            break;
+        case '-': 
+            cout << num1 - num2;  // 减法
+            break;
+        case '*': 
+            cout << num1 * num2;  // 乘法
+            break;
+        case '/': 
+            cout << num1 / num2;  // 除法（注意未处理除零错误）
+            break;
+        default:  // 无效运算符处理
+            cout << "Invalid operator";
+            break;
     }
 }
