21b1cc9bb0fad1cdff70cbd45818d732ac7dd2c6
CSSYJ-awa <13710671467@163.com>
2025-05-25 12:15:56 +0800

    解决合并冲突

--- try/a1.cpp
+++ try/a1.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
-    return 0;
+    float num1, num2;
+    char op;
+    cin>>num1>>num2>>op;
+    switch(op) {
+        case '+': cout<<num1+num2; break;
+        case '-': cout<<num1-num2; break;
+        case '*': cout<<num1*num2; break;
+        case '/': cout<<num1/num2; break;
+        default: cout<<"Invalid operator"; break;
+    }
 }
